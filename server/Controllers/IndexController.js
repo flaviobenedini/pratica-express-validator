f638ba45591e75dbaecd4540426d65f6a4a86755
Flavio Benedini <flavio.benedini@me.com>
2022-06-06 14:11:07 -0300

    feat: adicionando formulario na view index

--- server/Controllers/IndexController.js
+++ server/Controllers/IndexController.js
@@ -2,7 +2,7 @@ const controller = {}
 
 controller.index = (req, res) => res.render('index', { title: 'Express' })
 
-controller.add = (req,res) = res.send(req.body)
+controller.add = (req,res) => res.send(req.body)
 
 
 module.exports = controller
