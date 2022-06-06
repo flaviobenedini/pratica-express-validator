c09b6cf87f0c9c31bf3614f2f9d46668087df55f
Flavio Benedini <flavio.benedini@me.com>
2022-06-06 14:37:46 -0300

    feat: ajustando a rota e controller para tratar os erros vindos do formulário

--- server/Controllers/IndexController.js
+++ server/Controllers/IndexController.js
@@ -1,8 +1,14 @@
 const controller = {}
 
+const { validationResult } = require('express-validator');
+
+
+
 controller.index = (req, res) => res.render('index', { title: 'Express' })
 
-controller.add = (req,res) => res.send(req.body)
-
+controller.add = (req,res) => {
+    let erros = validationResult(req).mapped();    
+    res.render('index', { title: 'Express' }, erros, {valoresAntigos: req.body})
+}
 
 module.exports = controller
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,17 @@ const controller = require('../Controllers/IndexController')
 var express = require('express');
 var router = express.Router();
 
+const {check} = require('express-validator')
+
+const validatorIndex = [
+  check('nome', 'O nome precisa ser preenchido').not().isEmpty(),
+  check('email', 'Precisa ser digitado um email válido').isEmail(),
+  check('senha', 'A senha precisa ter, no mínimo 6 dígitos').isLength({min: 6})
+]
+
 /* GET home page. */
 router.get('/', controller.index)
-router.post('/', controller.add)
+router.post('/', validatorIndex, controller.add)
 
 
 module.exports = router;
