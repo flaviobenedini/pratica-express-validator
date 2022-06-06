5ef762affe7879ca13fa118c80d83c702e131fb0
Flavio Benedini <flavio.benedini@me.com>
2022-06-06 14:06:00 -0300

    feat: configurando a rota e controller post

--- server/Controllers/IndexController.js
+++ server/Controllers/IndexController.js
@@ -1,6 +1,8 @@
-const controller {}
+const controller = {}
 
 controller.index = (req, res) => res.render('index', { title: 'Express' })
+
+controller.add = (req,res) = res.send(req.body)
 
 
 module.exports = controller
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', controller.index);
+router.get('/', controller.index)
+router.post('/', controller.add)
+
 
 module.exports = router;
