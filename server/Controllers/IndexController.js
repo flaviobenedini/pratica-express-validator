e04ac672cf0c202ce2f7c3778ab436f226f3b1d3
Flavio Benedini <flavio.benedini@me.com>
2022-06-06 15:20:54 -0300

    feat: ajustando o controller para retornar pra pagina se ouver erro ou seguir se erros não existirem

--- server/Controllers/IndexController.js
+++ server/Controllers/IndexController.js
@@ -7,8 +7,12 @@ const { validationResult } = require('express-validator');
 controller.index = (req, res) => res.render('index', { title: 'Express' })
 
 controller.add = (req,res) => {
-    let erros = validationResult(req).mapped();    
-    res.render('index', { title: 'Express' }, erros, {valoresAntigos: req.body})
+    let erros = validationResult(req); 
+    if (!erros) {
+        res.render('index', { title: 'Express', erros: erros.errors, valoresAntigos: req.body})
+    } else {
+        res.send(req.body)
+    }
 }
 
 module.exports = controller
